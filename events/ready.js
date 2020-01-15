7a7106847a5fe14a712baa98850bf45d2c5a8492
Ayfri <34159136+Ayfri@users.noreply.github.com>
2020-01-15 17:57:35 +0100

    Translated the file

--- events/ready.js
+++ events/ready.js
@@ -1,5 +1,5 @@
 ﻿const chalk = require("chalk");
 
 module.exports = async (client) => {
-	console.log(chalk.greenBright(`${__filename.slice(__dirname.length + 1)}`)+chalk.reset(` : ${chalk.yellowBright(client.user.tag)} est allumé et présent sur ${chalk.magentaBright(client.guilds.size)} serveurs.`));
+	console.log(chalk.greenBright(`${__filename.slice(__dirname.length + 1)}`)+chalk.reset(` : ${chalk.yellowBright(client.user.tag)} is on and present on ${chalk.magentaBright(client.guilds.size)} serveurs.`));
 }
