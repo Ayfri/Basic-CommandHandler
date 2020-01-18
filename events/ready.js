c3e5fad60563467464aada584ceab050aefe0eea
Ayfri <34159136+Ayfri@users.noreply.github.com>
2020-01-18 02:12:49 +0100

    Fixed the translation

--- events/ready.js
+++ events/ready.js
@@ -1,5 +1,5 @@
 ﻿const chalk = require("chalk");
 
 module.exports = async (client) => {
-	console.log(chalk.greenBright(`${__filename.slice(__dirname.length + 1)}`)+chalk.reset(` : ${chalk.yellowBright(client.user.tag)} is on and present on ${chalk.magentaBright(client.guilds.size)} serveurs.`));
+	console.log(chalk.greenBright(`${__filename.slice(__dirname.length + 1)}`)+chalk.reset(` : ${chalk.yellowBright(client.user.tag)} is on and present on ${chalk.magentaBright(client.guilds.size)} guilds.`));
 }
