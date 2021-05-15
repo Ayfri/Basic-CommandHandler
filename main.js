5c198355b443a0970cd5224b4e2a81c09a948c34
Ayfri <pierre.ayfri@gmail.com>
2021-05-15 17:48:14 +0200

    fix: Translate strings.

--- main.js
+++ main.js
@@ -9,9 +9,9 @@ const configBOT = require('./informations/config');
 client.login(configBOT.token);
 client.writeFile = (path, object) => {
 	if (Object.entries(object).length ===
-	    0) throw new Error('L\'object que vous voulez enregistrer a un problème et est vide.\nPar sécurité une erreur a été créée et le fichier n\'a donc pas été enregistré.');
+	    0) throw new Error('The object you want to register has a problem and is empty. \nFor safety reasons an error was created and the file was therefore not saved.');
 	fs.writeFile(path, JSON.stringify(object, null, 4), err => {
-		if (err) return console.error(chalk.red('Une erreur au moment d\'enregistrer un fichier est arrivée :\n\n' + err.stack));
+		if (err) return console.error(chalk.red('An error occurred while saving a file :\n\n' + err.stack));
 	});
 };
 
@@ -21,11 +21,11 @@ let numberFiles = 0,
 
 // Loading JSONS files.
 fs.readdir('./informations/', (err, files) => {
-	console.log(chalk.red.bold('\n\nLancement du bot.\n\n'));
+	console.log(chalk.red.bold('\n\nStarting bot.\n\n'));
 	if (err) return console.error(err);
 	files.forEach(file => {
 		if (!file.endsWith('.json')) return;
-		console.log(chalk.white(`Fichier externe : `) + chalk.redBright(`${file}`));
+		console.log(chalk.white(`External file : `) + chalk.redBright(`${file}`));
 	});
 	numberFiles += files.length - 2;
 });
@@ -33,7 +33,7 @@ fs.readdir('./informations/', (err, files) => {
 // Loading events.
 fs.readdir('./events/', (err, files) => {
 	if (err) return console.error(err);
-	console.log(`\nÉvènements : (` + chalk.magenta.bold(`${files.length}`) + ')');
+	console.log(`\nEvents : (` + chalk.magenta.bold(`${files.length}`) + ')');
 	events = files.length;
 	files.forEach(file => {
 		if (!file.endsWith('.js')) return;
@@ -41,10 +41,10 @@ fs.readdir('./events/', (err, files) => {
 		let eventName = file.split('.')[0];
 		client.on(eventName, event.bind(null, client));
 		delete require.cache[require.resolve(`./events/${file}`)];
-		
-		console.log(chalk.white(`Chargement de l'évènement : `) + chalk.redBright(`${eventName}`));
+
+		console.log(chalk.white(`Loading the event : `) + chalk.redBright(`${eventName}`));
 	});
-	
+
 	numberFiles += files.length;
 });
 
@@ -53,13 +53,13 @@ client.aliases = new Enmap();
 
 // Loading commands.
 fs.readdir('./commands/', (err, files) => {
-	
+
 	if (err) return console.error(err);
-	console.log(`\nCommandes : (` + chalk.magenta.bold(`${files.length}`) + ')');
+	console.log(`\nCommands : (` + chalk.magenta.bold(`${files.length}`) + ')');
 	commands = files.length;
-	if (files.length <= 0) return console.log(chalk.red('== ERREUR ==\n\n Fichier : index.js \nAucun fichiers de commandes n\'a été trouvé.'));
+	if (files.length <= 0) return console.log(chalk.red('== ERROR ==\n\n File: index.js \n No commands files were found.'));
 	files.forEach(file => {
-		
+
 		if (!file.endsWith('js')) return;
 		let props = require(`./commands/${file}`);
 		let commandName = props.config.name;
@@ -67,15 +67,15 @@ fs.readdir('./commands/', (err, files) => {
 		props.config.aliases.forEach(alias => {
 			client.aliases.set(alias, props.config.name);
 		});
-		
-		
+
+
 		let aliases = props.config.aliases.map(e => e.toString()).join(', ');
-		console.log(chalk.white(`Chargement de la commande : `) + chalk.redBright(`${commandName}`));
-		console.log(chalk.white(`Raccourcis : `) + chalk.cyan(`${aliases}\n`));
+		console.log(chalk.white(`Loading the command : `) + chalk.redBright(`${commandName}`));
+		console.log(chalk.white(`Alises : `) + chalk.cyan(`${aliases}\n`));
 	});
-	
+
 	numberFiles += files.length;
-	console.log(chalk.white(`Chargement total de `) +
+	console.log(chalk.white(`Total loading of `) +
 	            chalk.magenta.bold(`${numberFiles}`) +
-	            chalk.white(` fichiers dont ${chalk.magenta.bold(commands)} commandes et ${chalk.magenta.bold(events)} évènements.`));
+	            chalk.white(` files including ${chalk.magenta.bold(commands)} commands and ${chalk.magenta.bold(events)} events.`));
 });
