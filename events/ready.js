2c025299b07f05db8f61d701d362fbe82b9b4f40
Ayfri <pierre.ayfri@gmail.com>
2021-05-15 17:39:12 +0200

    fix: Fix #28.

--- events/ready.js
+++ events/ready.js
@@ -1,6 +1,7 @@
 ﻿const chalk = require('chalk');
 
 module.exports = async (client) => {
-	console.log(chalk.greenBright(`${__filename.slice(__dirname.length + 1)}`) +
-	            chalk.reset(` : ${chalk.yellowBright(client.user.tag)} is on and present on ${chalk.magentaBright(client.guilds.size)} guilds.`));
+	console.log(
+		chalk.greenBright(`${__filename.slice(__dirname.length + 1)}`) + chalk.reset(` : ${chalk.yellowBright(client.user.tag)} is on and present on ${chalk.magentaBright(client.guilds.size)} guilds.`)
+	);
 };
